de7341652c84c2d5ff68f8054cecdf5de2bd7633
Aftab Alam Khan <aftab.khan@pager.com>
2021-09-17 12:16:55 -0400

    update the error handing

--- validatePhoneNumber.js
+++ validatePhoneNumber.js
@@ -25,17 +25,17 @@ class PhoneNumber {
         
         //validation for Area
         if ( (area.toString().length != 3) || (isNaN(area)) ) {
-            throw new InvalidAreaPhoneNumberError(`The area length is allowed only 3 and you passed '${area.toString().length}' and only numeric digit is allowed.`);
+            throw new InvalidAreaPhoneNumberError(`The area length is allowed only 3 and you passed '${area.toString().length}' and the area parameter only numeric is allowed.`);
         }
 
         //validation for serviceCode
         if ( (serviceCode.toString().length != 3) || (isNaN(serviceCode)) ) {
-            throw new InvalidServiceCodePhoneNumberError(`The serviceCode length is allowed only 3 and you passed ${serviceCode.toString().length} and only numeric digit is allowed.`);
+            throw new InvalidServiceCodePhoneNumberError(`The serviceCode length is allowed only 3 and you passed ${serviceCode.toString().length} and the serviceCode parameter only numeric is allowed.`);
         }
 
         //validation for subscriber
         if ( (subscriber.toString().length != 3) || (isNaN(subscriber)) ) {
-            throw new InvalidSubscriberPhoneNumberError(`The subscriber length is allowed only 4 and you passed ${subscriber.toString().length} and only numeric digit is allowed.`);
+            throw new InvalidSubscriberPhoneNumberError(`The subscriber length is allowed only 4 and you passed ${subscriber.toString().length} and the subscriber parameter only numeric is allowed.`);
         }
         this._area = area;
         this._serviceCode = serviceCode;
@@ -54,7 +54,7 @@ class PhoneNumber {
 }
 
 try {
-    const phone = new PhoneNumber('231', '4567', '78896');
+    const phone = new PhoneNumber('231', '456', '78896');
     console.log(phone.area);
     console.log(phone.serviceCode);
     console.log(phone.subscriber);
